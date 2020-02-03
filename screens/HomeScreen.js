6078763efb7c9d2967985ff12bb66751953b34e8
lichenfrost <36124344+lichenfrost@users.noreply.github.com>
2020-02-03 11:30:19 -0800

    Ejected project from Expo

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -17,7 +17,9 @@ export default function HomeScreen() {
   return (
 	
     <View style={styles.container}>
-	<WebView autosize={true} source = {{ uri: 'https://access.trubox.ca/'}}/>
+      
+      
+   
       <ScrollView
         style={styles.container}
         contentContainerStyle={styles.contentContainer}>
@@ -34,42 +36,22 @@ export default function HomeScreen() {
 
         <View style={styles.getStartedContainer}>
           <DevelopmentModeNotice />
+        
+	        
 
           <Text style={styles.getStartedText}>Get started by opening</Text>
 
-          <View
-            style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-            <MonoText>screens/HomeScreen.js</MonoText>
-          </View>
+          
 
           <Text style={styles.getStartedText}>
             Change this text and your app will automatically reload.
           </Text>
-        </View>
 
-        <View style={styles.helpContainer}>
-          <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-            <Text style={styles.helpLinkText}>
-              Help, it didn’t automatically reload!
-            </Text>
-          </TouchableOpacity>
-        </View>
-		
-      </ScrollView>
-		
-      <View style={styles.tabBarInfoContainer}>
-        <Text style={styles.tabBarInfoText}>
-          This is a tab bar. You can edit it in:
-        </Text>
-		
-        <View
-          style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-          <MonoText style={styles.codeHighlightText}>
-            navigation/MainTabNavigator.js
-          </MonoText>
-        </View>
-      </View>
+        </View> 
+            <WebView style={ styles.contentContainer } autosize={true} source = {{ uri: 'https://access.trubox.ca#document'}}/>
 			
+      </ScrollView>
+      
     </View>
 	
   );

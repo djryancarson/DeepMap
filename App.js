32b69f1f2749cfb89062c2ab465ba2c6773a5fd7
lichenfrost <36124344+lichenfrost@users.noreply.github.com>
2020-02-05 22:25:08 -0800

    initial prototype with map page and tab buttons which lead to empty pages

--- App.js
+++ App.js
@@ -1,12 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React from 'react';
+import * as React from 'react';
 import WebView from 'react-native-webview';
 import {
   SafeAreaView,
@@ -15,6 +7,8 @@ import {
   View,
   Text,
   StatusBar,
+  Button,
+  Picker
 } from 'react-native';
 
 import {
@@ -25,19 +19,90 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React$Node = () => {
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+//homescreen contains the map 
+function Resources ( {navigation} ){
   return (
-    <View style = {styles.container}>
-         <WebView
-         source = {{ uri:
-         'https://access.trubox.ca/'}}
-         />
+    <View style={styles.HomeScreen}>
+      
+      <View style={{height:50,borderBottomColor:'lightgrey',borderBottomWidth:2}}>
+        <Picker>
+          <Picker.Item label = "All" value = "all" />
+          <Picker.Item label = "Academic Accessibility & Supports" value = "academic" />
+          <Picker.Item label = "Counselling" value = "counselling" />
+          <Picker.Item label = "Disability" value = "disability" />
+        </Picker>
+
+        
       </View>
+
+      <WebView  style={styles.mapView}
+        source = {{ uri:
+        'https://access.trubox.ca/#wpv-view-layout-825-TCPID925'}}
+      />
+
+        <View style={{height:50,borderTopColor:'lightgrey',borderTopWidth:2}}>
+        
+        </View>
+
+      
+
+
+      
+  
+
+      <View style={styles.tabBar}>
+        <Button style={styles.tabButton} type='outline' color='lightgrey'  title="Community " onPress={() => navigation.navigate('Community')}/>
+        <Button style={styles.tabButton} type='outline' color='lightgrey'  title="Personal" onPress={() => navigation.navigate('Personl')}/>
+        <Button style={styles.tabButton} type='outline' color='lightgrey'  title="Preferences" onPress={() => navigation.navigate('Preferences')}/>
+      </View>
+    </View>
+    
+
+
+  );
+}
+
+function Community (){
+  return (
+    <View>
+      <Text>Hello! This is the community page!</Text>
+    </View>
+  );
+}
+function Preferences (){
+  return (
+    <View>
+      <Text>Hello! This is the preferences page!</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App ( { navigation } ) {
+  return (
+    <NavigationContainer>
+         <Stack.Navigator>
+           
+           <Stack.Screen name="Resources" component={Resources} />
+           <Stack.Screen name="Community" component={Community} />
+           <Stack.Screen name="Preferences" component={Preferences} />
+         </Stack.Navigator>
+         
+         
+    </NavigationContainer>
+    
   );
 };
 
+
 const styles = StyleSheet.create({
   scrollView: {
+    flex: 6,
     backgroundColor: Colors.lighter,
   },
   engine: {
@@ -57,7 +122,7 @@ const styles = StyleSheet.create({
     color: Colors.black,
   },
   container: {
-      height: 1000,
+      height: 500,
    },
   sectionDescription: {
     marginTop: 8,
@@ -76,6 +141,39 @@ const styles = StyleSheet.create({
     paddingRight: 12,
     textAlign: 'right',
   },
+  tabBar: {
+    flex:0.2,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'stretch',
+    borderTopColor: 'lightgrey',
+    borderTopWidth:2
+  },
+
+
+  mapView: {
+    flex: 1,
+    backgroundColor: 'white'
+
+  },
+  HomeScreen: {
+    flex:1,
+    flexDirection: 'column',
+    //height: 1000
+  },
+
+  scrollView: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black',
+    height: 500,
+    
+  },
+  tabButton: {
+    flexDirection: 'row',
+    alignSelf: 'center',
+    textAlignVertical: 'center',
+  }
 });
 
 export default App;
